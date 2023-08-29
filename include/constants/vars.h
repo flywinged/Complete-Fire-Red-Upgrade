6b1a0857b91605eefa8619e0f5e23af8dea7a546
Clayton Brown <clayton.john.brown@gmail.com>
2023-08-29 15:03:15 -0700

    Automatic Type Charts

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -94,7 +94,7 @@
 #define VAR_ENIGMA_BERRY_AVAILABLE         0x4033
 
 #define VAR_0x4034                         0x4034 // Used to track whether or not the player can access the key items for healing and pc
-#define VAR_RESORT_GOREGEOUS_STEP_COUNTER  0x4035
+#define VAR_RESORT_GOREGEOUS_STEP_COUNTER  0x4035 // Used to trigger the player leaving the clearing
 #define VAR_RESORT_GORGEOUS_REQUESTED_MON  0x4036
 #define VAR_PC_BOX_TO_SEND_MON             0x4037
 #define VAR_FANCLUB_FAN_COUNTER            0x4038
